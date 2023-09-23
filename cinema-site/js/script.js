8f44c90fc292d9df21d4665ca0342b60a13f0c9f
Aleksey <61293074+traycer255@users.noreply.github.com>
2023-09-24 03:07:34 +0500

    Переименовал картинки

--- cinema-site/js/script.js
+++ cinema-site/js/script.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 $(document).ready(function () {
   $('.slider').slick({
     dots: true,
   })
+=======
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+  })
+>>>>>>> 326ebd3967666f049547fad76c147112b44bf27e
 })
